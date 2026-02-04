7f110c13d0e6f69ba18685cc2ed0a8b12ce1d11d
brunosgs <brunosoges@gmail.com>
2026-02-04 19:01:46 -0300

    aula 0507
    feat: implementada solução de operações matemáticas, proposta pelo professor para praticar a criação de endpoints.

--- src/main/java/com/github/brunosgs/controllers/MathController.java
+++ src/main/java/com/github/brunosgs/controllers/MathController.java
@@ -19,6 +19,73 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping("/sub/{numberOne}/{numberTwo}")
+    public Double sub(@PathVariable String numberOne,
+                      @PathVariable String numberTwo) {
+        if (isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/mult/{numberOne}/{numberTwo}")
+    public Double mult(@PathVariable String numberOne,
+                       @PathVariable String numberTwo) {
+        if (isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping("/div/{numberOne}/{numberTwo}")
+    public Double div(@PathVariable String numberOne,
+                      @PathVariable String numberTwo) {
+        if (isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        Double division = convertToDouble(numberOne) / convertToDouble(numberTwo);
+
+        if (division.isNaN()) {
+            throw new UnsupportedMathOperationException("Undefined result, not a number (NaN).");
+        }
+
+        return division;
+    }
+
+    @RequestMapping("/average/{numberOne}/{numberTwo}")
+    public Double average(@PathVariable String numberOne,
+                          @PathVariable String numberTwo) {
+        if (isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        Double average = (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+
+        if (average.isNaN()) {
+            throw new UnsupportedMathOperationException("Undefined result, not a number (NaN).");
+        }
+
+        return average;
+    }
+
+    @RequestMapping("/square/{number}")
+    public Double square(@PathVariable String number) {
+        if (isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        Double resultSquareRoot = Math.sqrt(convertToDouble(number));
+
+        if (resultSquareRoot.isNaN()) {
+            throw new UnsupportedMathOperationException("Undefined result, not a number (NaN).");
+        }
+
+        return resultSquareRoot;
+    }
+
     private Double convertToDouble(String strNumber) {
         if (strNumber == null || strNumber.isEmpty()) {
             throw new UnsupportedMathOperationException("Please set a numeric value!");
